7028c7dda33e43a8c92d9cbc0ca6b42ae1c1d5be
georges ryssen <georges.ryssen@gmail.com>
2022-11-15 16:45:58 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+# generate 20 users
+100.times do
+  Article.create!(
+    title: Faker::Restaurant.name,
+    content: Faker::Restaurant.description
+  )
+end
